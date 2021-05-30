f4a3684b8b2fb27ea12e7591b7379fc107ec8a2a
FahimAdib <fahim.adib01@gmail.com>
2021-05-30 14:29:54 +0600

    added all functionality

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,10 @@ const LOCAL_STORAGE_KEY = "cookingWithReact.recipes";
 
 function App() {
   const [recipes, setRecipes] = useState(sampleRecipes);
+  const [selectedRecipeId, setSelectedRecipeId] = useState();
+  const selectedRecipe = recipes.find(
+    (recipe) => recipe.id === selectedRecipeId
+  );
 
   useEffect(() => {
     const recipeJSON = localStorage.getItem(LOCAL_STORAGE_KEY);
@@ -22,29 +26,45 @@ function App() {
   const recipeContextValue = {
     handleRecipeAdd: handleRecipeAdd,
     handleRecipeDelete: handleRecipeDelete,
+    handleRecipeSelect: handleRecipeSelect,
+    handleRecipeChange: handleRecipeChange,
   };
+
+  function handleRecipeChange(id, recipe) {
+    const newRecipes = [...recipes];
+    const index = newRecipes.findIndex((r) => r.id === id);
+    newRecipes[index] = recipe;
+    setRecipes(newRecipes);
+  }
+  function handleRecipeSelect(id) {
+    setSelectedRecipeId(id);
+  }
 
   function handleRecipeAdd() {
     const newRecipe = {
       id: uuidv4(),
-      name: "New",
-      servings: 1,
-      cookTime: "1:00",
-      instructions: "Instr.",
-      ingredients: [{ id: uuidv4(), name: "Name", amount: "1 Tbs" }],
+      name: "",
+      servings: "",
+      cookTime: "",
+      instructions: "",
+      ingredients: [{ id: uuidv4(), name: "", amount: "" }],
     };
 
     setRecipes([...recipes, newRecipe]);
+    setSelectedRecipeId(newRecipe.id);
   }
 
   function handleRecipeDelete(id) {
-    setRecipes(recipes.filter((recipe) => recipe.id != id));
+    if (selectedRecipeId !== null && selectedRecipeId === id) {
+      setSelectedRecipeId(undefined);
+    }
+    setRecipes(recipes.filter((recipe) => recipe.id !== id));
   }
 
   return (
     <RecipeContext.Provider value={recipeContextValue}>
       <RecipeList recipes={recipes} />
-      <RecipeEdit />
+      {selectedRecipe && <RecipeEdit recipe={selectedRecipe} />}
     </RecipeContext.Provider>
   );
 }
