d12fd2b32ab1fa7d3891969367abc1d80ef82431
FahimAdib <fahim.adib01@gmail.com>
2021-05-30 13:16:24 +0600

    JSX for entire project

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { useState, createContext, useEffect } from "react";
 import RecipeList from "./RecipeList";
+import RecipeEdit from "./RecipeEdit";
 import "../css/App.css";
+import { v4 as uuidv4 } from "uuid";
+
+export const RecipeContext = createContext();
+const LOCAL_STORAGE_KEY = "cookingWithReact.recipes";
 
 function App() {
-  return <RecipeList recipes={sampleRecipes} />;
+  const [recipes, setRecipes] = useState(sampleRecipes);
+
+  useEffect(() => {
+    const recipeJSON = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (recipeJSON !== null) setRecipes(JSON.parse(recipeJSON));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(recipes));
+  }, [recipes]);
+
+  const recipeContextValue = {
+    handleRecipeAdd: handleRecipeAdd,
+    handleRecipeDelete: handleRecipeDelete,
+  };
+
+  function handleRecipeAdd() {
+    const newRecipe = {
+      id: uuidv4(),
+      name: "New",
+      servings: 1,
+      cookTime: "1:00",
+      instructions: "Instr.",
+      ingredients: [{ id: uuidv4(), name: "Name", amount: "1 Tbs" }],
+    };
+
+    setRecipes([...recipes, newRecipe]);
+  }
+
+  function handleRecipeDelete(id) {
+    setRecipes(recipes.filter((recipe) => recipe.id != id));
+  }
+
+  return (
+    <RecipeContext.Provider value={recipeContextValue}>
+      <RecipeList recipes={recipes} />
+      <RecipeEdit />
+    </RecipeContext.Provider>
+  );
 }
 
 const sampleRecipes = [
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import Recipe from "./Recipe";
 
+import { RecipeContext } from "./App";
+
 export default function RecipeList({ recipes }) {
+  const { handleRecipeAdd } = useContext(RecipeContext);
   return (
     <div className="recipe-list">
       <div>
@@ -10,7 +13,9 @@ export default function RecipeList({ recipes }) {
         })}
       </div>
       <div className="recipe-list-add-recipe-btn-container">
-        <button className="btn btn-primary">Add Recipe</button>
+        <button className="btn btn-primary" onClick={handleRecipeAdd}>
+          Add Recipe
+        </button>
       </div>
     </div>
   );
