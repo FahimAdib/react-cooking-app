836978922ca647a006ad23961074b0f645c9d67e
FahimAdib <fahim.adib01@gmail.com>
2021-05-30 15:12:52 +0600

    Updates

---
+++ precache-manifest.9395321ee3f088f6133bc8502274e771.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c57982272c5952cc7a6ad6fe5fdbe12",
+    "url": "/react-cooking-app/index.html"
+  },
+  {
+    "revision": "5ba22ebd4aa5b36c3e2c",
+    "url": "/react-cooking-app/static/css/main.ee169e70.chunk.css"
+  },
+  {
+    "revision": "21569e9229a3e1cdfdd2",
+    "url": "/react-cooking-app/static/js/2.d9eb3301.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-cooking-app/static/js/2.d9eb3301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ba22ebd4aa5b36c3e2c",
+    "url": "/react-cooking-app/static/js/main.d3f0758b.chunk.js"
+  },
+  {
+    "revision": "096603e2bcf5762cb59a",
+    "url": "/react-cooking-app/static/js/runtime-main.24e9daac.js"
+  }
+]);
